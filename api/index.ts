2169aee6965903d28935ae34795fae4efd574998
Adrien Pringle <30927609+AdrienPringle@users.noreply.github.com>
2023-07-09 20:11:26 -0400

    use express to reduce serverless function count

--- api/index.ts
+++ api/index.ts
@@ -1,25 +1,26 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
+import express from 'express';
 import { PrismaClient } from '@prisma/client'
 import admin from "firebase-admin";
 import { initializeApp } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 
+const app = express();
 
 // prisma
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 // firebase auth
-const app = initializeApp({
+const firebase_app = initializeApp({
   credential: admin.credential.cert({
     projectId: "chromascape",
     privateKey: JSON.parse(process.env.FIREBASE_PRIVATE_KEY || ""),
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   })
 });
-const defaultAuth = getAuth(app);
+const defaultAuth = getAuth(firebase_app);
 
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+app.get("/api/hello", async (req, res) => {
   const { name = 'World' } = req.query
 
   const user = await defaultAuth.getUserByEmail("adrienpringle@gmail.com");
@@ -38,4 +39,6 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   return res.json({
     message: `Hello ${name}, ${db_user.firebase_uid}, ${db_user.createdAt}!`,
   })
-}
+})
+
+module.exports = app;
