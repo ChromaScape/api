9bbf048a0183e0a6306c38d87b3521cd53d7a6c4
Adrien Pringle <30927609+AdrienPringle@users.noreply.github.com>
2023-07-10 01:33:43 -0400

    basic crud

--- api/index.ts
+++ api/index.ts
@@ -25,7 +25,7 @@ enum Role {
 }
 interface Context {
   uid?: string
-  id?: BigInt
+  id?: bigint
 }
 
 declare global {
@@ -99,24 +99,259 @@ async function authorizeDevice(req: Request, res: Response, next: NextFunction) 
   return next(err);
 }
 
-// user apis (todo)
-app.get("/api/user", getFirebaseAuth, authorizeUser, async (req, res) => { });
-app.patch("/api/user", getFirebaseAuth, authorizeUser, async (req, res) => { });
-app.post("/api/user/device", getFirebaseAuth, authorizeUser, async (req, res) => { });
-app.delete("/api/user/device", getFirebaseAuth, authorizeUser, async (req, res) => { });
-app.post("/api/user/pattern", getFirebaseAuth, authorizeUser, async (req, res) => { });
-app.delete("/api/user/pattern", getFirebaseAuth, authorizeUser, async (req, res) => { });
-app.post("/api/user/pattern_schedule", getFirebaseAuth, authorizeUser, async (req, res) => { });
-app.delete("/api/user/pattern_schedule", getFirebaseAuth, authorizeUser, async (req, res) => { });
+// user apis
+
+// no settings so not useful yet
+// app.patch("/api/user", getFirebaseAuth, authorizeUser, async (req, res) => { });
+
+// for now, no handshake. let the device be the sole decider of who does what
+// app.post("/api/user/device", getFirebaseAuth, authorizeUser, async (req, res) => { });
+
+app.delete("/api/user/device", getFirebaseAuth, authorizeUser, async (req, res, next) => {
+  const device_id = req.body.device_id as string | undefined;
+  const user_id = req.context.id;
+
+  if (!user_id || !device_id) {
+    const err = new Error('no device id specified');
+    res.status(400);
+    return next(err);
+  }
+
+  try {
+    const updated = await prisma.device.updateMany({
+      where: {
+        id: BigInt(device_id),
+        userId: user_id,
+      }, data: {
+        userId: null
+      }
+    })
+
+    if (updated.count == 0) {
+      const err = new Error('unable to update device');
+      res.status(400);
+      return next(err);
+    }
+
+    return res.json("success: removed device user");
+
+  } catch (e) {
+    return next(e)
+  }
 
 
-// device apis (todo)
-app.get("/api/device", getFirebaseAuth, authorizeDevice, async (req, res) => { });
-app.post("/api/device/user", getFirebaseAuth, authorizeDevice, async (req, res) => { });
-app.delete("/api/device/user", getFirebaseAuth, authorizeDevice, async (req, res) => { });
+
+});
+app.post("/api/user/pattern", getFirebaseAuth, authorizeUser, async (req, res, next) => {
+  const content = req.body.content as string | undefined;
+  const user_id = req.context.id;
+
+  if (!user_id) {
+    const err = new Error('no uid found??');
+    res.status(500);
+    return next(err);
+  }
+
+  try {
+    const new_pattern = await prisma.pattern.create({
+      data: {
+        userId: user_id,
+        content
+      }
+    });
+
+
+    return res.json(new_pattern);
+  } catch (e) {
+    return next(e);
+  }
+});
+
+app.delete("/api/user/pattern", getFirebaseAuth, authorizeUser, async (req, res, next) => {
+  const pattern_id = req.body.pattern_id as string | undefined;
+  const user_id = req.context.id;
+
+  if (!user_id || !pattern_id) {
+    const err = new Error('no pattern id specified');
+    res.status(400);
+    return next(err);
+  }
+
+  try {
+    const deleted = await prisma.pattern.deleteMany({
+      where: {
+        id: BigInt(pattern_id),
+        userId: user_id
+      }
+    })
+
+    if (deleted.count == 0) {
+      const err = new Error('unable to delete pattern');
+      res.status(400);
+      return next(err);
+    }
+
+    return res.json("success: deleted pattern");
+
+  } catch (e) {
+    return next(e)
+  }
+});
+
+app.post("/api/user/pattern_schedule", getFirebaseAuth, authorizeUser, async (req, res, next) => {
+  const device_id = req.body.device_id as string | undefined;
+  const pattern_id = req.body.pattern_id as string | undefined;
+  const scheduled_time = req.body.scheduled_time as string | undefined;
+
+  if (!device_id) {
+    const err = new Error('no device id specified');
+    res.status(400);
+    return next(err);
+  }
+
+  if (!pattern_id) {
+    const err = new Error('no pattern id specified');
+    res.status(400);
+    return next(err);
+  }
+
+  if (!scheduled_time) {
+    const err = new Error('no scheduled time specified');
+    res.status(400);
+    return next(err);
+  }
+
+  const user_id = req.context.id;
+
+  if (!user_id) {
+    const err = new Error('no uid found??');
+    res.status(500);
+    return next(err);
+  }
+
+  try {
+    //todo disallow scheduling for a device you don't own
+    //todo validate scheduled time
+    const new_scheduled_pattern = prisma.scheduledPattern.create({
+      data: {
+        deviceId: BigInt(device_id),
+        patternId: BigInt(pattern_id),
+        scheduledFor: scheduled_time,
+      }
+    });
+
+    return res.json(new_scheduled_pattern);
+
+  } catch (e) {
+    return next(e)
+  }
+});
+app.delete("/api/user/pattern_schedule", getFirebaseAuth, authorizeUser, async (req, res, next) => {
+  const scheduled_pattern_id = req.body.scheduled_pattern_id as string | undefined;
+  const user_id = req.context.id;
+
+  if (!user_id || !scheduled_pattern_id) {
+    const err = new Error('no scheduled pattern id specified');
+    res.status(400);
+    return next(err);
+  }
+
+  try {
+    //todo disbale deleteing for devices u dont own
+    const deleted = await prisma.scheduledPattern.delete({
+      where: {
+        id: BigInt(scheduled_pattern_id),
+      }
+    });
+
+    return res.json("success: deleted pattern");
+
+
+  } catch (e) {
+    return next(e)
+  }
+
+});
+
+
+// device apis
+app.post("/api/device/user", getFirebaseAuth, authorizeDevice, async (req, res, next) => {
+  const user_id = req.body.user_id as string | undefined;
+  const device_id = req.context.id;
+
+  if (!user_id || !device_id) {
+    const err = new Error('no user id specified');
+    res.status(400);
+    return next(err);
+  }
+
+  try {
+    const updated = await prisma.device.updateMany({
+      where: {
+        id: device_id,
+        userId: null,
+      }, data: {
+        userId: BigInt(user_id)
+      }
+    })
+
+    if (updated.count == 0) {
+      const err = new Error('unable to update device');
+      res.status(400);
+      return next(err);
+    }
+
+    return res.json("success: updated device user");
+
+  } catch (e) {
+    return next(e)
+  }
+});
 
 
 // global apis
+app.get("/api/user", getFirebaseAuth, async (req, res, next) => {
+
+  const uid = req.context.uid;
+  if (!uid) {
+    const err = new Error('No uid found??');
+    res.status(500);
+    return next(err);
+  }
+
+  try {
+    const user = await prisma.user.findUniqueOrThrow({
+      where: {
+        firebase_uid: uid
+      }
+    });
+    return res.json(user);
+  } catch (e) {
+    next(e)
+  }
+});
+
+app.get("/api/device", getFirebaseAuth, async (req, res, next) => {
+  const uid = req.context.uid;
+  if (!uid) {
+    const err = new Error('No uid found??');
+    res.status(500);
+    return next(err);
+  }
+
+  try {
+    const device = await prisma.device.findUniqueOrThrow({
+      where: {
+        firebase_uid: uid
+      }
+    });
+    return res.json(device);
+  } catch (e) {
+    next(e)
+  }
+});
+
+
 app.post("/api/user", getFirebaseAuth, async (req, res, next) => {
   const uid = req.context.uid;
   if (!uid) {
@@ -125,13 +360,17 @@ app.post("/api/user", getFirebaseAuth, async (req, res, next) => {
     return next(err);
   }
 
-  const new_user = await prisma.user.create({
-    data: {
-      firebase_uid: uid
-    }
-  });
+  try {
+    const new_user = await prisma.user.create({
+      data: {
+        firebase_uid: uid
+      }
+    });
 
-  return res.json(new_user);
+    return res.json(new_user);
+  } catch (e) {
+    next(e)
+  }
 });
 
 app.get("/api/pattern/:pattern_id", async (req, res, next) => {
